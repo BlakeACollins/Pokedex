86b359687036da1904e456ae65a28e8b9aece825
Blake Collins <blakeac369@gmail.com>
2020-10-13 14:09:06 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Pokedex from './Pokedex';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+     <div className="App">
+        <Pokedex
+        />
+    </div>
+    )
+  }
+}
+
+export default App;
