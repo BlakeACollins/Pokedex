3e436a39d97c6fc1c5ef409685517a5b56243e4f
Blake Collins <blakeac369@gmail.com>
2020-10-15 15:05:50 -0700

    Add files via upload

---
+++ src/Pokegame.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Pokegame extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Pokegame!</h1>
+            </div>
+        );
+    }
+}
+
+export default Pokegame
